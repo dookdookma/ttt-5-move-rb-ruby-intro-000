716a7794375fb116997f768853d85891fc6cecce
Luke Rollins <ldrollins@gmail.com>
2021-02-18 18:09:07 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,5 +7,9 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input = gets.strip
-  
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
